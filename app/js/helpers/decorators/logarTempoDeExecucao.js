1f8718a102e9ce3c7ac16a0a9de78b42d12633cb
Marcos Ribeiro <mrj84@outlook.com>
2020-03-12 07:01:14 -0300

    feat(benchmark): check methods execution time

--- app/js/helpers/decorators/logarTempoDeExecucao.js
+++ app/js/helpers/decorators/logarTempoDeExecucao.js
@@ -1,11 +1,23 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    function logarTempoDeExecucao() {
-        return function (target, porpertyKey, descriptor) {
+    function logarTempoDeExecucao(emSegundos = false) {
+        return function (target, propertyKey, descriptor) {
             const metodoOriginal = descriptor.value;
             descriptor.value = function (...args) {
+                let unidade = 'ms';
+                let divisor = 1;
+                if (emSegundos) {
+                    unidade = 's';
+                    divisor = 1000;
+                }
+                console.log('==========================');
+                console.log(`Params passados para o método ${propertyKey}: ${JSON.stringify(args)}`);
+                const t1 = performance.now();
                 const retorno = metodoOriginal.apply(this, args);
+                const t2 = performance.now();
+                console.log(`O retorno do método ${propertyKey} é: ${JSON.stringify(retorno)}`);
+                console.log(`O método ${propertyKey} demorou: ${(t1 - t2) / divisor} ${unidade}`);
                 return retorno;
             };
             return descriptor;
